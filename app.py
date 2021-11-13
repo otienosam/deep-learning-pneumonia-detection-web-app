de8e3b82b5c341aa4013d4266b1559cde4b19e81
otienosam <61335701+otienosam@users.noreply.github.com>
2021-11-13 18:05:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,25 +53,30 @@ def index():
     # Main page
     return render_template('index.html')
 def dicom2png(dicom_file,output_folder):
+    try:
+        ds = pydicom.dcmread(dicom_file)
+        shape = ds.pixel_array.shape
 
-    ds = pydicom.dcmread(dicom_file)
-    shape = ds.pixel_array.shape
+        # Convert to float to avoid overflow or underflow losses.
+        image_2d = ds.pixel_array.astype(float)
 
-    # Convert to float to avoid overflow or underflow losses.
-    image_2d = ds.pixel_array.astype(float)
+        # Rescaling grey scale between 0-255
+        image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
 
-    # Rescaling grey scale between 0-255
-    image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
+        # Convert to uint
+        image_2d_scaled = np.uint8(image_2d_scaled)
 
-    # Convert to uint
-    image_2d_scaled = np.uint8(image_2d_scaled)
+        # Write the PNG file
+        # with open(f'{dicom_file.strip(".dcm")}.png', 'wb') as png_file:
+        with open(os.path.join(output_folder,dicom_file)+'.png' , 'wb') as png_file:
+            w = png.Writer(shape[1], shape[0], greyscale=True)
+            w.write(png_file, image_2d_scaled)
+            print('dicom successfuly converted')
+    except:
+        print("could not convert dicom file")
 
-    # Write the PNG file
-    # with open(f'{dicom_file.strip(".dcm")}.png', 'wb') as png_file:
-    with open(os.path.join(output_folder,dicom_file)+'.png' , 'wb') as png_file:
-        w = png.Writer(shape[1], shape[0], greyscale=True)
-        w.write(png_file, image_2d_scaled)
-        print('file successfuly converted')
+
+    
       
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
@@ -89,9 +94,11 @@ def upload():
         output_folder = os.path.join(basepath,'uploads')
         if file_path.endswith(".dcm"):
             dicom2png(file_path,output_folder)
-             
-        preds = model_predict(file_path, model)
-        os.remove(file_path)#removes file from the server after prediction has been returned
+        
+        list_of_files = os.listdir(source_folder)
+        preds = model_predict(list_of_files[1], model)
+        os.remove(list_of_files[1])#removes file from the server after prediction has been returned
+        os.remove(list_of_files[0])
 
         
             
