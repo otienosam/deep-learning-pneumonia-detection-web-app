86b6fc439c7a6564ca3cab2001c068911a747507
otienosam <61335701+otienosam@users.noreply.github.com>
2021-11-13 14:17:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import glob
 import re
 import numpy as np
+import png, pydicom
 
 # Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
@@ -50,6 +51,29 @@ def model_predict(img_path, model):
 def index():
     # Main page
     return render_template('index.html')
+def dicom2png(dicom_file):
+    try:
+        ds = pydicom.dcmread(dicom_file)
+        shape = ds.pixel_array.shape
+
+        # Convert to float to avoid overflow or underflow losses.
+        image_2d = ds.pixel_array.astype(float)
+
+        # Rescaling grey scale between 0-255
+        image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
+
+        # Convert to uint
+        image_2d_scaled = np.uint8(image_2d_scaled)
+
+        # Write the PNG file
+        with open(f'{dicom_file.strip(".dcm")}.png', 'wb') as png_file:
+        # with open(os.path.join(output_folder,file)+'.png' , 'wb') as png_file:
+            w = png.Writer(shape[1], shape[0], greyscale=True)
+            w.write(png_file, image_2d_scaled)
+    except:
+        print('Could not convert: ', file)
+    return w
+
 
 
 @app.route('/predict', methods=['GET', 'POST'])
@@ -66,9 +90,13 @@ def upload():
 	
 	
         # Make prediction
-        print('this is a prediction model')
-        preds = model_predict(file_path, model)
-        os.remove(file_path)#removes file from the server after prediction has been returned
+        try:
+            preds = model_predict(file_path, model)
+            os.remove(file_path)#removes file from the server after prediction has been returned
+        except PIL.UnidentifiedImageError:
+            w = dicom2png(file_path)
+            preds = model_predict(w, model)
+            os.remove(w)
 
         # Arrange the correct return according to the model. 
 		# In this model 1 is Pneumonia and 0 is Normal.
