9cf9605ea23c1a84cca4a5e92c17d3181efa8083
otienosam <61335701+otienosam@users.noreply.github.com>
2021-11-15 11:28:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿from __future__ import division, print_function
+from __future__ import division, print_function
 # coding=utf-8
 import sys
 import os
@@ -6,7 +6,6 @@ import glob
 import re
 import numpy as np
 import png, pydicom
-import PIL
 
 # Keras
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
@@ -35,44 +34,48 @@ print('Model loaded. Start serving...')
 #model = ResNet50(weights='imagenet')
 #print('Model loaded. Check http://127.0.0.1:5000/')
 
+
 def model_predict(img_path, model):
     img = image.load_img(img_path, target_size=(64, 64)) #target_size must agree with what the trained model expects!!
 
     # Preprocessing the image
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
+
    
     preds = model.predict(img)
     return preds
 
-def dicom2png(dicom_file,output_folder):
-    try:
-        ds = pydicom.dcmread(dicom_file)
-        shape = ds.pixel_array.shape
+def dicom2png(source_folder, output_folder):
+    list_of_files = os.listdir(source_folder)
+    for file in list_of_files:
+        if file.endswith('.md'):
+            continue
+        try:
+            ds = pydicom.dcmread(os.path.join(source_folder,file))
+            shape = ds.pixel_array.shape
 
-        # Convert to float to avoid overflow or underflow losses.
-        image_2d = ds.pixel_array.astype(float)
+            # Convert to float to avoid overflow or underflow losses.
+            image_2d = ds.pixel_array.astype(float)
 
-        # Rescaling grey scale between 0-255
-        image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
+            # Rescaling grey scale between 0-255
+            image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
 
-        # Convert to uint
-        image_2d_scaled = np.uint8(image_2d_scaled)
+            # Convert to uint
+            image_2d_scaled = np.uint8(image_2d_scaled)
 
-        # Write the PNG file
-        # with open(f'{dicom_file.strip(".dcm")}.png', 'wb') as png_file:
-        with open(os.path.join(output_folder,dicom_file)+'.png' , 'wb') as png_file:
-            w = png.Writer(shape[1], shape[0], greyscale=True)
-            w.write(png_file, image_2d_scaled)
-            print('dicom successfuly converted')
-    except:
-        print("could not convert dicom file")
-    
+            # Write the PNG file
+            with open(os.path.join(output_folder,file)+'.png' , 'wb') as png_file:
+                w = png.Writer(shape[1], shape[0], greyscale=True)
+                w.write(png_file, image_2d_scaled)
+        except:
+            print('Could not convert: ', file)
+
 @app.route('/', methods=['GET'])
 def index():
     # Main page
-    return render_template('index.html')    
-      
+    return render_template('index.html')
+
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
@@ -84,33 +87,19 @@ def upload():
         file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 	
+	
         # Make prediction
-
-        #if its dicom file
-        output_folder = os.path.join(basepath,'uploads')
-        list_of_files = os.listdir(output_folder)
-        for i in list_of_files:
-            if i.endswith('.png'):
-                continue
-            if i.endswith('.jpg'):
-                continue
-            if i.endswith('.jpeg'):
-                continue
-            if i.endswith('.dcm'):
-                continue
-            else:
-                os.remove(i)
-                
-        print(len(list_of_files))
-
+        input_folder = os.path.join(basepath,'uploads')
+        output_folder = os.path.join(basepath,'dicom_png')
 
         str1 = 'Pneumonia'
         str2 = 'Normal'
         if file_path.endswith(".dcm"):
-            dicom2png(file_path,output_folder)
-            print(len(list_of_files))
-            preds = model_predict(list_of_files[1], model)
-            os.remove(list_of_files[1])#removes file from the server after prediction has been returned
+            dicom2png(input_folder,output_folder)
+            dicom_path = os.path.join(basepath, 'dicom_png', secure_filename(f.filename))
+            f.save(dicom_path)
+            preds = model_predict(dicom_path, model)
+            os.remove(dicom_path)#removes file from the server after prediction has been returned
             if preds == 1:
                 return str1
             else:
@@ -123,9 +112,6 @@ def upload():
                 return str1
             else:
                 return str2
-        # Arrange the correct return according to the model. 
-		#In this model 1 is Pneumonia and 0 is Normal.
-
     return None
 
     #this section is used by gunicorn to serve the app on Heroku
@@ -135,5 +121,3 @@ if __name__ == '__main__':
     # Serve the app with gevent 
     #http_server = WSGIServer(('', 5000), app)
     #http_server.serve_forever()
-
-    
